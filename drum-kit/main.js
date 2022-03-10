75130a10b63667bef21358b4fa69753fbd9b4425
akhmadjon-dev <info.ahmadjon@gmail.com>
2022-03-10 18:39:42 +0500

    completed with keyCodes

--- drum-kit/main.js
+++ drum-kit/main.js
@@ -4,3 +4,34 @@ function play(title){
     var audio = new Audio('sounds/'+title+'.wav');
     audio.play();
 }
+
+window.addEventListener('keydown', function(event){
+    var key = event.keyCode;
+    if(key == 65){
+        play('clap');
+    }
+    else if(key == 83){
+        play('hihat');
+    }
+    else if(key == 68){
+        play('kick');
+    }
+    else if(key == 70){
+        play('openhat');
+    }
+    else if(key == 71){
+        play('boom');
+    }
+    else if(key == 72){
+        play('ride');
+    }
+    else if(key == 74){
+        play('snare');
+    }
+    else if(key == 75){
+        play('tom');
+    }
+    else if(key == 76){
+        play('tink');
+    }
+});
